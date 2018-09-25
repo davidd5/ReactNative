e186729d7662c010c7746cdc52f5725650e99488
daavvvs <D16126656@mydit.ie>
2018-09-25 20:03:01 +0200

    Initial push

---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import AlbumDetail from './AlbumDetail'
+
+class AlbumList extends Component {
+  state = { albums: [] };
+  componentWillMount() {
+    fetch('https://rallycoding.herokuapp.com/api/music_albums')
+    /*.then((response) => response.json())
+    .then((responseData) => {
+      console.log(responseData);
+    });*/
+    .then(response => response.json())
+    .then(data => this.setState({ albums: data }));
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail key={album.title} album={album} />
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <View>
+        {this.renderAlbums()}
+      </View>
+    );
+  }
+}
+
+export default AlbumList;
